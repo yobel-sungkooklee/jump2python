45e5a4c203a6c4ebf9808680b93e060bacb476bb
yobel-sungkooklee <yobellee@g.skku.edu>
2025-01-01 04:42:26 +0000

    Edit

--- 05-1/calculator3.py
+++ 05-1/calculator3.py
@@ -6,11 +6,16 @@ class Calculator:
     def add(self, num):
         self.result += num
         return self.result
+    
+    def sub(self, num):
+        self.result -= num
+        return self.result
 
 cal1 = Calculator()
 cal2 = Calculator()
 
 print(cal1.add(3))
 print(cal1.add(4))
-print(cal2.add(3))
+print(cal2.add(6))
 print(cal2.add(7))
+print(cal2.sub(55))
---
+++ 05-1/calculator4.py
@@ -0,0 +1,10 @@
+# calculator3.py
+class FourCal:
+    def setdata(self,first,second):
+        self.first=first
+        self.second=second
+        
+a=FourCal()
+a.setdata(4,3)
+#FourCal.setdata(a,5,2)
+print(a.first)
