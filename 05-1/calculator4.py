7655e2753e0ff8fed1a298fd18b293e91670140f
yobel-sungkooklee <yobellee@g.skku.edu>
2025-01-01 04:53:45 +0000

    Edition by YOBEL

--- 05-1/calculator4.py
+++ 05-1/calculator4.py
@@ -3,8 +3,31 @@ class FourCal:
     def setdata(self,first,second):
         self.first=first
         self.second=second
-        
+    def add(self):
+        return self.first+self.second
+    def mul(self):
+        result=self.first * self.second
+        return result
+    def sub(self):
+        result=self.first-self.second
+        return result
+    def div(self):
+        result= self.first/self.second
+        return result
+    
+    
+
 a=FourCal()
-a.setdata(4,3)
+a.setdata(4,2)
 #FourCal.setdata(a,5,2)
-print(a.first)
+b=FourCal()
+b.setdata(3,8)
+print(a.add())
+print(a.sub())
+print(a.mul())
+print(a.div())
+
+print(b.add())
+print(b.sub())
+print(b.mul())
+print(b.div())
